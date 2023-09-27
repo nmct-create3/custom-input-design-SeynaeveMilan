c929ecb134d876a6bfdd7ee11af15a96fbe5b4f1
SeynaeveMilan <milan.seynaeve@student.howest.be>
2023-09-27 15:23:31 +0200

    s

--- script/app.js
+++ script/app.js
@@ -1,5 +1,17 @@
 const handlePasswordSwitcher = function () {
     console.log("handlePasswordSwitcher executed");
+    const pwdToggle = document.querySelector('.js-password-toggle-checkbox');
+    pwdToggle.addEventListener('change', function(){
+        const passwordInput = document.querySelector('.js-password-input')
+        const checkbox = document.querySelector('.js-password-toggle-checkbox').checked;
+        if(checkbox == true){
+            passwordInput.type = "text";
+        }else{
+            passwordInput.type = "password";
+        }
+
+    })
+    
 }
 
 const init = function () {
